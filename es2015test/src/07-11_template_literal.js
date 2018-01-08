f97bf3d8dc0559d57894ec9fe19d82d8feb2f177
Reinkaos <ds2lvg@gmail.com>
2018-01-08 13:01:49 +0900

    템플릿 리터럴 사용 : `${변수명}`

---
+++ es2015test/src/07-11_template_literal.js
@@ -0,0 +1,9 @@
+var d1 = new Date();
+var name = "안지현";
+var r1 = `${name} 님에게 ${d1.toDateString() }에 연락했다.`;
+console.log(r1);
+
+var product = "아이폰7";
+var price = 920000;
+var str = `${product}의 가격은 ${price}입니다.`;
+console.log(str);
