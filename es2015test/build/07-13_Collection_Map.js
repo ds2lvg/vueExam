483a504f01f8f9f874c85808211dc7206859d7dc
Reinkaos <ds2lvg@gmail.com>
2018-01-08 13:03:30 +0900

    컬렉션의 Map

---
+++ es2015test/build/07-13_Collection_Map.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Map은 key, value => key는 앨범
+var teams = new Map();
+teams.set("삼성 썬더스", "2015~2017");
+teams.set("SK 나이츠", "2017~");
+teams.set("삼성생명 블루밍스", "2015~2017");
+teams.set("KEB하나은행", "2017~");
+teams.set("우리카드 위비", "2017~");
+teams.set("히어로즈", "2017~");
+
+console.log(teams.has("LG")); // false
+console.log(teams.get("삼성 썬더스")); // "2015~2017"
+// clear(), delete() 메서드도 존재함
