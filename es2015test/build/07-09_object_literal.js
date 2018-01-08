1867cab777d0bcbb1f491b5ac8fbe09bb4c69aa5
Reinkaos <ds2lvg@gmail.com>
2018-01-08 10:51:20 +0900

    객체 리터럴

---
+++ es2015test/build/07-09_object_literal.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var name = "안지현";
+var age = 22;
+var email = "ajh@test.com";
+
+// 객체 속성명과 변수명과 동일하면 생략 가능
+// var obj = { name:name, age:age, email:email };
+var obj = { name: name, age: age, email: email };
+console.log(obj);
