9feeca777fe6172c9813310709c45553292c6168
Reinkaos <ds2lvg@gmail.com>
2018-01-12 10:22:32 +0900

    컴포넌트에서 스타일(css) 적용)

---
+++ styletest/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      <h2>{{msg}}</h2>
+      <child1></child1>
+      <child2></child2>
+  </div>
+</template>
+
+<script>
+import Child1 from './components/Child1.vue'
+import Child2 from './components/Child2.vue'
+
+export default {
+    name: 'app',
+    components: { Child1, Child2 },
+    data() {
+        return {
+            msg: 'Welcome to your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ styletest/src/components/Child1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">{{msg}}</div>
+</template>
+
+<script>
+export default {
+    name: ' child1',
+    data() {
+        return {
+            msg: 'Child1'
+        }
+    }
+}
+</script>
+<style>
+.main{ border: 1px solid #000; background-color: yellow }
+</style>
